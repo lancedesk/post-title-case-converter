0fd9b8a16e81940c3877e11adb43605d21825fa4
Lance Desk <136012772+lancedesk@users.noreply.github.com>
2024-09-10 01:13:02 +0300

    Update post-title-case-converter-functions.php to add nonce and validation and verification.

--- includes/post-title-case-converter-functions.php
+++ includes/post-title-case-converter-functions.php
@@ -1,18 +1,26 @@
 <?php
-// Enqueue necessary scripts for AJAX
-function post_case_converter_enqueue_scripts() {
+/* Enqueue necessary scripts for AJAX */
+function ptc_converter_enqueue_scripts()
+{
     wp_enqueue_script('jquery');
-    wp_enqueue_script('post-case-converter-script', plugin_dir_url(__FILE__) . '../js/scripts.js', array('jquery'));
-    wp_localize_script('post-case-converter-script', 'postCaseConverter', array('ajaxUrl' => admin_url('admin-ajax.php')));
-    wp_enqueue_style('post-case-converter-style', plugin_dir_url(__FILE__) . '../css/style.css');
+    wp_enqueue_script('ptc_converter-script', plugin_dir_url(__FILE__) . '../js/scripts.js', array('jquery'), null, true);
+    wp_enqueue_style('ptc_converter-style', plugin_dir_url(__FILE__) . '../css/style.css');
+
+    /* Localize the script with AJAX URL and nonce */
+    wp_localize_script('ptc_converter-script', 'ajax_data', [
+        'ajaxurl' => admin_url('admin-ajax.php'),
+        'nonce'   => wp_create_nonce('ptc_converter_nonce')
+    ]);
 }
 
-// Enqueue necessary scripts and styles for the plugin
-add_action('admin_enqueue_scripts', 'post_case_converter_enqueue_scripts');
+/* Enqueue necessary scripts and styles for the plugin */
+add_action('admin_enqueue_scripts', 'ptc_converter_enqueue_scripts');
 
-// Function to convert title case
-function convert_title_case($title, $case) {
-    switch ($case) {
+/* Function to convert title case */
+function convert_title_case($title, $case)
+{
+    switch ($case)
+    {
         case 'lowercase':
             return strtolower($title);
         case 'uppercase':
